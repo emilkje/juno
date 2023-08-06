d11730aabdd8561661462cb91617d7d9d5035f72
Emil Kjelsrud <emil.kjelsrud@intility.no>
2023-08-06 22:07:14 +0200

    improve the suggest improvements command

--- src/commands/suggestImprovementsCommand.ts
+++ src/commands/suggestImprovementsCommand.ts
@@ -17,14 +17,15 @@ import {
  * @returns A Promise that resolves when the code generation is complete.
  */
 export const suggestImprovementsCommand = createCommand('juno.suggestImprovements', async (context) => {
-	const instructions = `$USER will ask both generic and specific questions that you will try to answer as best as possible. 
+	const instructions = `$USER will ask you how to improve their code. You should use the provided code (if any) and respond with practical solutions.
 
 Always adhere to the folling rules:
 
 1. Respond in markdown format
 2. When providing code blocks you have to qualify it with what language. e.g \`\`\`javascript or \`\`\`typescript.
 3. Always answer as $ASSISTANT and avoid using phrases as "as a large language model" etc.
-4. Use the scratchpad if relevant to the question`;
+4. Use the scratchpad if relevant to the question
+5. Apply common structural/creational/behavioural software patterns to your edits when relevant`;
 
 	const systemMessage = createSystemMessage(context, instructions);
 	console.log('using system message', systemMessage);
